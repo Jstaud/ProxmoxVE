5be53701b5059f87da836cda0939d38caaa95434
James S <james@jamesstaud.com>
2025-03-04 16:18:38 -0600

    feat: update ente installation script to use pct exec for all commands

--- ct/ente.sh
+++ ct/ente.sh
@@ -25,35 +25,31 @@ function update_script() {
     check_container_resources
 
     msg_info "Updating ${APP} LXC"
-    $STD apt-get update
-    $STD apt-get -y upgrade
+    pct exec $CTID -- apt-get update
+    pct exec $CTID -- apt-get -y upgrade
 
     msg_info "Installing Docker"
-    $STD apt-get install -y docker.io
+    pct exec $CTID -- apt-get install -y docker.io
     msg_ok "Docker installed successfully"
 
     msg_info "Cloning ${APP} repository"
-    git clone https://github.com/ente-io/ente /opt/ente
-    cd /opt/ente/server
+    pct exec $CTID -- git clone https://github.com/ente-io/ente /opt/ente
     msg_ok "Repository cloned successfully"
 
     msg_info "Starting ${APP} server with Docker Compose"
-    $STD docker compose up --build -d
+    pct exec $CTID -- docker compose -f /opt/ente/server/docker-compose.yml up --build -d
     msg_ok "${APP} server started successfully"
 
     msg_info "Installing npm and yarn"
-    $STD apt-get install -y nodejs npm
-    $STD npm install -g yarn
+    pct exec $CTID -- apt-get install -y nodejs npm
+    pct exec $CTID -- npm install -g yarn
     msg_ok "npm and yarn installed successfully"
 
     msg_info "Setting up ${APP} web client"
-    cd /opt/ente/web
-    git submodule update --init --recursive
-    $STD yarn install
-    NEXT_PUBLIC_ENTE_ENDPOINT=http://localhost:8080 $STD yarn build
+    pct exec $CTID -- bash -c "cd /opt/ente/web && git submodule update --init --recursive && yarn install && NEXT_PUBLIC_ENTE_ENDPOINT=http://localhost:8080 yarn build"
     msg_ok "${APP} web client set up successfully"
 
-    if [[ ! -d /opt/ente ]]; then
+    if ! pct exec $CTID -- test -d /opt/ente; then
       msg_error "No ${APP} Installation Found!"
       exit
     fi
@@ -71,4 +67,4 @@ IP=$(pct exec $CTID -- ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8080${CL}"
